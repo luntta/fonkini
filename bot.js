17dfa0d1b4a9f812065dde267e57abe58e86a0ea
Raine Luntta <raine.luntta@gmail.com>
2020-04-24 11:11:15 +0300

    Changes .env files to load only outside of prod environment

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
-require("dotenv").config({ path: __dirname + "/.env" });
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config({ path: __dirname + "/.env" });
+}
 
 const Discord = require("discord.js");
 const cfg = require("./config.json");
